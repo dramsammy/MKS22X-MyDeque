ea84e5c5d6d4dd8ff0a791d9051637bd626eaf56
dramsammy <dramsammy@stuy.edu>
2019-03-22 10:33:26 -0400

    working on add first

--- MyDeque.java
+++ MyDeque.java
@@ -13,28 +13,35 @@ public class MyDeque<E>{
     return data.length;
   }
   public String toString(){
-    String returnValue = "[";
+    String returnValue = "{";
     if (start>end){
       for (int i = start; i < data.length; i++){
         returnValue+= data[i];
-        returnValue += ",";
+        returnValue += " ";
       }
       for (int i = 0; i < end; i++){
         returnValue+= data[i];
-        returnValue += ",";
+        returnValue += " ";
       }
-      returnValue+= data[end] + "]";
+      returnValue+= data[end] + "}";
       }
     else{
       for (int i = start; i < end; i++){
         returnValue+= data[i];
-        returnValue += ",";
+        returnValue += " ";
       }
-      returnValue+= data[end] + "]";
+      returnValue+= data[end] + "}";
     }
     return returnValue;
   }
-  public void addFirst(E element){ }
+  public void addFirst(E element){
+    if (element == null){
+      throw new NullPointerException("Element is null");
+    }
+    if (start == 0 && end == data.length){
+      
+    }
+  }
   public void addLast(E element){ }
   public E removeFirst(){ }
   public E removeLast(){ }
