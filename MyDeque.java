99bb8ef775ad106b944179c4f2d4f35bca7c2314
Dramsammy <dramsammy@stuy.edu>
2019-03-25 00:10:15 -0400

    working on my deque

--- MyDeque.java
+++ MyDeque.java
@@ -28,38 +28,52 @@ public class MyDeque<E>{
         returnValue += " ";
       }
       for (int i = 0; i < end; i++){
+        if(i == end - 1){
+          returnValue+= data[end] + "}";
+        }
+        else{
         returnValue+= data[i];
         returnValue += " ";
       }
-      returnValue+= data[end] + "}";
       }
+    }
     else{
       for (int i = start; i < end; i++){
+        if(i == end - 1){
+          returnValue+= data[end] + "}";
+        }
+        else{
         returnValue+= data[i];
         returnValue += " ";
       }
-      returnValue+= data[end] + "}";
-    }
-    return returnValue;
+
+      }
+
   }
+  return returnValue;
+}
+
   @SuppressWarnings("unchecked")
   public void addFirst(E element){
     if (element == null){
       throw new NullPointerException("Element is null");
     }
-    if (start == 0 && end == data.length){
+    if (size() == data.length){
       resize();
     }
-    if (size != 0 && start == 0){
+    if (size != 0){
+      if (start == 0){
       start = data.length - 1;
     }
-    if (size != 0 && start != 0){
+    else{
       start--;
     }
+  }
     data[start] = element;
     size++;
 
   }
+
   public void addLast(E element){
     if (element == null) {
       throw new NullPointerException();
@@ -105,5 +119,29 @@ public class MyDeque<E>{
       throw new NoSuchElementException();
     }
     return data[end];
+  }
+  public void resize() {
+    E[] temp = (E[]) new Object[data.length * 2 + 1];
+    int a = 0;
+    for (int i = start; i < data.length && (start > end || (start <= end && i <= end)); i++) {
+      temp[a] = data[i];
+      a++;
+    }
+    if (start > end) {
+      for (int i = 0; i <= end; i++) {
+      temp[a] = data[i];
+      a++;
+     }
+   }
+    data = temp;
+    start = 0;
+    end = a;
+}
+  public static void main(String[] args) {
+    MyDeque<Integer> Test1 = new MyDeque<>();
+    for (int i = 0; i < 41; i+= 3) {
+      Test1.addFirst(i);
+    }
+    System.out.println(Test1);
   }
 }
