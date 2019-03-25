4ec54ee01f0703b91a5038ff5a26129987dec465
dramsammy <dramsammy@stuy.edu>
2019-03-25 10:55:42 -0400

    worked on calculator

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,15 @@
 public class Calculator extends MyDeque{
+    private static double answer;
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
       String[] str = s.split();
       MyDeque a = new MyDeque();
+      for (int i = 0; i < str.length; i++){
+        a.add(i);
+      }
+
     }
+    public static double EvalH()
 }
