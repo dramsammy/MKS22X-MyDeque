a80121e1e966ecabae648985f2b940a0873d0eda
dramsammy <dramsammy@stuy.edu>
2019-03-25 10:50:18 -0400

    beginning my deque

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator extends MyDeque{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    public static double eval(String s){
+      String[] str = s.split();
+      MyDeque a = new MyDeque();
+    }
+}
